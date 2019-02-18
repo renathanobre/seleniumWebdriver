dbfd0ed9ca1b7ad85420ca4650671233abe1d917
Renata Nobre <renathanore@gmail.com>
2019-02-18 01:46:07 -0800

    executescript javascriptexecutor sample

---
+++ MavenSeleniumWDriverProject/src/test/java/com/my/webdriver/ExecuteJavascript.java
@@ -0,0 +1,46 @@
+/**
+ * 
+ */
+package com.my.webdriver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+
+/**
+ * @author renat
+ *
+ */
+public class ExecuteJavascript {
+
+	/**
+	 * @param args
+	 * @throws InterruptedException 
+	 */
+	public static void main(String[] args) throws InterruptedException {
+
+
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Renata\\projetos\\app\\chromedriver.exe");
+
+		WebDriver driver = new ChromeDriver();
+
+		driver.get("https://formy-project.herokuapp.com/modal");
+
+		WebElement modalButton = driver.findElement(By.id("modal-button"));
+		modalButton.click();
+
+
+		WebElement modalCloseButton = driver.findElement(By.id("close-button"));
+
+		JavascriptExecutor js = (JavascriptExecutor)driver;	
+		js.executeScript("arguments[0].click();", modalCloseButton);
+
+		Thread.sleep(1000);
+		driver.quit();
+
+	}
+
+}
