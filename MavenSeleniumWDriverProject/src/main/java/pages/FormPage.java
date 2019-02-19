7c4f2dfa180fee86b20121013edb19d1392519c5
Renata Nobre <renathanore@gmail.com>
2019-02-18 19:31:29 -0800

    refactoring

--- MavenSeleniumWDriverProject/src/main/java/pages/FormPage.java
+++ MavenSeleniumWDriverProject/src/main/java/pages/FormPage.java
@@ -1,5 +1,22 @@
 package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+
 public class FormPage {
+	
+	public void submitForm(WebDriver driver){
+
+		driver.findElement(By.id("first-name")).sendKeys("John");
+		driver.findElement(By.id("last-name")).sendKeys("Doe");
+		driver.findElement(By.id("job-title")).sendKeys("QA Engineer");
+		driver.findElement(By.id("radio-button-2")).click();
+		driver.findElement(By.id("checkbox-2")).click();
+		driver.findElement(By.cssSelector("option[value='3']")).click();
+		driver.findElement(By.id("datepicker")).sendKeys("02/02/2019");
+		driver.findElement(By.id("datepicker")).sendKeys(Keys.RETURN);
+		driver.findElement(By.cssSelector(".btn.btn-lg.btn-primary")).click();
+	}
 
 }
--- MavenSeleniumWDriverProject/src/test/java/com/my/webdriver/Form.java
+++ MavenSeleniumWDriverProject/src/test/java/com/my/webdriver/Form.java
@@ -5,10 +5,10 @@ package com.my.webdriver;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
+
+import pages.ConfirmationPage;
+import pages.FormPage;
+
 import static org.junit.Assert.assertEquals;
 
 /**
@@ -31,41 +31,19 @@ public class Form {
 
 		driver.get("https://formy-project.herokuapp.com/form");
 
-		submitForm(driver);
+		FormPage formPage = new FormPage();
+		formPage.submitForm(driver);
 
-		waitFormAlertBanner(driver);
+		ConfirmationPage confirmationPage = new ConfirmationPage();
+		confirmationPage.waitFormAlertBanner(driver);
 
 		assertEquals("The form was successfully submitted!"
-				, getAlertBannerText(driver));
+				, confirmationPage.getAlertBannerText(driver));
 
 		driver.quit();
 
 	}
 
-	public static void submitForm(WebDriver driver){
-
-		driver.findElement(By.id("first-name")).sendKeys("John");
-		driver.findElement(By.id("last-name")).sendKeys("Doe");
-		driver.findElement(By.id("job-title")).sendKeys("QA Engineer");
-		driver.findElement(By.id("radio-button-2")).click();
-		driver.findElement(By.id("checkbox-2")).click();
-		driver.findElement(By.cssSelector("option[value='3']")).click();
-		driver.findElement(By.id("datepicker")).sendKeys("02/02/2019");
-		driver.findElement(By.id("datepicker")).sendKeys(Keys.RETURN);
-		driver.findElement(By.cssSelector(".btn.btn-lg.btn-primary")).click();
-	}
-
-	public static void waitFormAlertBanner(WebDriver driver){
-
-		WebDriverWait wait = new WebDriverWait(driver,10);
-		wait.until(ExpectedConditions
-				.visibilityOfElementLocated(By.className("alert")));
-	}
-	
-	public static String getAlertBannerText(WebDriver driver){
-		
-		return driver.findElement(By.className("alert")).getText();
-	}
 
 
 }
