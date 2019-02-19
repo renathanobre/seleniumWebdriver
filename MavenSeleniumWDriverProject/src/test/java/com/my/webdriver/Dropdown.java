d5f1e18489fc21c60a1564a208b69aab37a91597
Renata Nobre <renathanore@gmail.com>
2019-02-18 18:00:17 -0800

    dropdown

---
+++ MavenSeleniumWDriverProject/src/test/java/com/my/webdriver/Dropdown.java
@@ -0,0 +1,46 @@
+/**
+ * 
+ */
+package com.my.webdriver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.By;
+
+/**
+ * @author renat
+ *
+ */
+public class Dropdown {
+
+	/**
+	 * @param args
+	 * @throws InterruptedException 
+	 */
+	public static void main(String[] args) throws InterruptedException {
+
+		
+		System.setProperty("webdriver.chrome.driver",
+	            "C:\\Renata\\projetos\\app\\chromedriver.exe");
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://formy-project.herokuapp.com/dropdown");
+		
+		Thread.sleep(1000);
+		
+		WebElement dropdownMenu = driver.findElement(By.id("dropdownMenuButton"));
+		dropdownMenu.click();
+		Thread.sleep(1000);
+		
+		WebElement autocompleteOption = driver.findElement(By.id("autocomplete"));
+		autocompleteOption.click();
+		Thread.sleep(1000);
+		
+				
+		//driver.quit();
+		
+	}
+
+}
